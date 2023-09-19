365c609c77a18bef5e50759ab47982de85b21ac8
Mark Dijkstra <mark.dijkstra2@hva.nl>
2023-09-19 11:35:04 +0200

    sellDateUpdater gemaakt

--- frontend/auc-vue/src/router/index.js
+++ frontend/auc-vue/src/router/index.js
@@ -1,38 +1,19 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
+
 import WelcomePage from "@/components/WelcomeComponent.vue";
 import OffersOverview31Component from "@/components/offers/OffersOverview31Component.vue";
-
 import offersOverview32 from "@/components/offers/OffersOverview32.vue";
 
 export const router = createRouter({
     history: createWebHashHistory(),
     routes: [
-        {
-            path: "/",
-            component: WelcomePage,
-        },
-        {
-            path: "/offers",
-            component: OffersOverview31Component,
-        },
-        {
-            path: "/offeractive",
-            component: offersOverview32,
-        },
-        {
-            path: "/offerhistory",
-
-        },
-        {
-            path: "/myaccount",
-
-        }, {
-            path: "/login",
-        },
-        {
-            path: "/signup",
-
-        },
+        { path: "/", component: WelcomePage },
+        { path: "/offers", component: OffersOverview31Component },
+        { path: "/offeractive", component: offersOverview32 },
+        { path: "/offerhistory", },
+        { path: "/myaccount", },
+        { path: "/login", },
+        { path: "/signup", },
 
     ]
 });
