782e335d8bee125ed2ccb855381de83211c2d896
Ryan-Jay Takkenberg <Ryan-jay.takkenb erg@hva.nl>
2023-10-02 15:52:16 +0200

    alert buttons toegevoegd

--- frontend/auc-vue/src/components/offers/OffersDetail34.vue
+++ frontend/auc-vue/src/components/offers/OffersDetail34.vue
@@ -40,11 +40,26 @@
         </tr>
         </tbody>
       </table>
-      <button @click="handleSave" :disabled="!hasChanged">Save</button>
-      <button @click="handleCancel" :disabled="hasChanged">Cancel</button>
-      <button @click="handleReset" :disabled="!hasChanged" >Reset</button>
-      <button @click="handleClear">Clear</button>
-      <button @click="deleteDetails">Delete</button>
+<!--      <button @click="handleSave" :disabled="!hasChanged">Save</button>-->
+<!--      <button @click="handleCancel" :disabled="hasChanged">Cancel</button>-->
+<!--      <button @click="handleReset" :disabled="!hasChanged" >Reset</button>-->
+<!--      <button @click="handleClear">Clear</button>-->
+<!--      <button @click="deleteDetails">Delete</button>-->
+      <button @click="confirmation('Save')" :disabled="!hasChanged">Save</button>
+      <button @click="confirmation('Clear')" :disabled="!hasChanged">Clear</button>
+      <button @click="confirmation('Reset')" :disabled="!hasChanged">Reset</button>
+      <button @click="confirmation('Cancel')" :disabled="hasChanged">Cancel</button>
+      <button @click="confirmation('Delete')">Delete</button>
+
+      <div v-if="isConfirmationVisible" class="confirmation-dialog">
+        <div class="confirmation-content">
+          <p v-if="this.confirmAction === 'Delete' ">Are you sure you want to delete this action on id {{ this.selectedOffer.id }}</p>
+          <p v-else>Are you sure you want to perform this action on id {{ this.selectedOffer.id }}</p>
+          <button @click="confirm">Confirm</button>
+          <button @click="cancel">Cancel</button>
+        </div>
+      </div>
+
 
     </div>
   </div>
@@ -61,49 +76,86 @@ export default {
       offerStatusArray: Object.values(Offer.Status),
       selectedOffer: null,
       copiedOffer: null,
+      isConfirmationVisible: false,
+      confirmAction: String,
     }
   },
   methods: {
     deleteDetails() {
       this.$emit('delete-offer', this.selectedOffer);
+    },    // handleSave() {
+    //   // TODO oplossing vinden voor de date
+    //   this.selectedOffer.title = this.copiedOffer.title;
+    //   this.selectedOffer.description = this.copiedOffer.description;
+    //   this.selectedOffer.status = this.copiedOffer.status;
+    //   this.selectedOffer.sellDate = this.copiedOffer.sellDate;
+    //   this.selectedOffer.valueHighestBid = this.copiedOffer.valueHighestBid;
+    //   this.$router.push(this.$route.matched[0].path);
+    //   this.$emit('Select-parent-class', this.selectedOffer);
+    // },
+    // handleCancel() {
+    //   this.copiedOffer.title = this.selectedOffer.title;
+    //   this.copiedOffer.description = this.selectedOffer.description;
+    //   this.copiedOffer.status = this.selectedOffer.status;
+    //   this.copiedOffer.sellDate = this.selectedOffer.sellDate;
+    //   this.copiedOffer.valueHighestBid = this.selectedOffer.valueHighestBid;
+    //   this.$router.push(this.$route.matched[0].path);
+    //   this.$emit('Select-parent-class', this.selectedOffer);
+    // },
+    // handleClear() {
+    //   this.copiedOffer.title = null;
+    //   this.copiedOffer.description = null;
+    //   this.copiedOffer.status = null;
+    //   this.copiedOffer.sellDate = null;
+    //   this.copiedOffer.valueHighestBid = null;
+    // },
+    // handleReset() {
+    //   this.copiedOffer.title = this.selectedOffer.title;
+    //   this.copiedOffer.description = this.selectedOffer.description;
+    //   this.copiedOffer.status = this.selectedOffer.status;
+    //   this.copiedOffer.sellDate = this.selectedOffer.sellDate;
+    //   this.copiedOffer.valueHighestBid = this.selectedOffer.valueHighestBid;
+    // },
+
+    confirmation(action) {
+      this.confirmAction = action;
+      this.isConfirmationVisible = true;
     },
-    handleSave(){
-      // TODO oplossing vinden voor de date
-      this.selectedOffer.title = this.copiedOffer.title;
-      this.selectedOffer.description = this.copiedOffer.description;
-      this.selectedOffer.status =  this.copiedOffer.status;
-      this.selectedOffer.sellDate = this.copiedOffer.sellDate;
-      this.selectedOffer.valueHighestBid = this.copiedOffer.valueHighestBid;
-      this.$router.push(this.$route.matched[0].path);
-      this.$emit('Select-parent-class', this.selectedOffer);
+    confirm() {
+
+      if (this.confirmAction === "Clear") {
+        this.copiedOffer.title = null;
+        this.copiedOffer.description = null;
+        this.copiedOffer.status = null;
+        this.copiedOffer.sellDate = null;
+        this.copiedOffer.valueHighestBid = null;
+      } else if (this.confirmAction === "Reset") {
+        this.copiedOffer.title = this.selectedOffer.title;
+        this.copiedOffer.description = this.selectedOffer.description;
+        this.copiedOffer.status = this.selectedOffer.status;
+        this.copiedOffer.sellDate = this.selectedOffer.sellDate;
+        this.copiedOffer.valueHighestBid = this.selectedOffer.valueHighestBid;
+      } else if (this.confirmAction === "Cancel") {
+        this.copiedOffer.title = this.selectedOffer.title;
+        this.copiedOffer.description = this.selectedOffer.description;
+        this.copiedOffer.status = this.selectedOffer.status;
+        this.copiedOffer.sellDate = this.selectedOffer.sellDate;
+        this.copiedOffer.valueHighestBid = this.selectedOffer.valueHighestBid;
+        this.$router.push(this.$route.matched[0].path);
+        this.$emit('Select-parent-class', this.selectedOffer);
+      }else if (this.confirmAction === "Save"){
+        this.selectedOffer.title = this.copiedOffer.title;
+        this.selectedOffer.description = this.copiedOffer.description;
+        this.selectedOffer.status = this.copiedOffer.status;
+        this.selectedOffer.sellDate = this.copiedOffer.sellDate;
+        this.selectedOffer.valueHighestBid = this.copiedOffer.valueHighestBid;
+        this.$router.push(this.$route.matched[0].path);
+        this.$emit('Select-parent-class', this.selectedOffer);
+      }else if (this.confirmAction === "Delete"){
+        this.$emit('delete-offer', this.selectedOffer);
+      }
+      this.isConfirmationVisible = false;
     },
-    handleCancel(){
-      this.copiedOffer.title = this.selectedOffer.title;
-      this.copiedOffer.description = this.selectedOffer.description;
-      this.copiedOffer.status = this.selectedOffer.status;
-      this.copiedOffer.sellDate = this.selectedOffer.sellDate;
-      this.copiedOffer.valueHighestBid = this.selectedOffer.valueHighestBid;
-      this.$router.push(this.$route.matched[0].path);
-      this.$emit('Select-parent-class', this.selectedOffer);
-    },
-    handleClear(){
-      this.copiedOffer.title = null;
-      this.copiedOffer.description = null;
-      this.copiedOffer.status = null;
-      this.copiedOffer.sellDate = null;
-      this.copiedOffer.valueHighestBid = null;
-    },
-    handleReset(){
-      this.copiedOffer.title = this.selectedOffer.title;
-      this.copiedOffer.description = this.selectedOffer.description;
-      this.copiedOffer.status = this.selectedOffer.status;
-      this.copiedOffer.sellDate = this.selectedOffer.sellDate;
-      this.copiedOffer.valueHighestBid = this.selectedOffer.valueHighestBid;
-    }
-  },
-  created(){
-    this.selectedOffer = this.offerList.find(offer => offer.id === parseInt(this.$route.params.id));
-    this.copiedOffer = Offer.copyConstructor(this.selectedOffer);
 
   },
   computed: {
@@ -133,13 +185,44 @@ export default {
           this.selectedOffer = this.offerList.find(offer => offer.id === parseInt(this.$route.params.id));
           this.copiedOffer = Offer.copyConstructor(this.selectedOffer);
         }
-      }
+      },created(){
+    this.selectedOffer = this.offerList.find(offer => offer.id === parseInt(this.$route.params.id))
+    this.copiedOffer = Offer.copyConstructor(this.selectedOffer);
+  },
 
 
 };
 </script>
 
 <style scoped>
+.confirmation-dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.confirmation-content {
+  background: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+
+.confirmation-content p {
+  margin-bottom: 10px;
+  font-size: 16px;
+}
+
+.confirmation-content button {
+  margin-left: 10px;
+  width: 20%;
+}
 .inputfieldText {
   width: 97.5%;
 }
