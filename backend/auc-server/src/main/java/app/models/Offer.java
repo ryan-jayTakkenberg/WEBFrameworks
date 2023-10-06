37df9f27d9a306bd5148f222d4c0bf3060637f3d
Mark Dijkstra <mark.dijkstra2@hva.nl>
2023-10-06 21:52:28 +0200

    setup backend

--- backend/auc-server/src/main/java/app/models/Offer.java
+++ backend/auc-server/src/main/java/app/models/Offer.java
@@ -1,4 +1,49 @@
 package app.models;
 
+import java.time.LocalDate;
+
 public class Offer {
+    private int id;
+    private String title;
+    private String status;
+    private String description;
+    private LocalDate sellDate;
+    private double valueHighestBid;
+
+    public Offer(int id, String title, String status, String description, LocalDate sellDate, double valueHighestBid) {
+        this.id = id;
+        this.title = title;
+        this.status = status;
+        this.description = description;
+        this.sellDate = sellDate;
+        this.valueHighestBid = valueHighestBid;
+    }
+
+    public Offer(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getSellDate() {
+        return sellDate;
+    }
+
+    public double getValueHighestBid() {
+        return valueHighestBid;
+    }
 }
--- backend/auc-server/src/main/java/app/rest/offersController.java
+++ backend/auc-server/src/main/java/app/rest/offersController.java
@@ -1,4 +1,21 @@
 package app.rest;
 
+import app.models.Offer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/offers")
 public class offersController {
+
+    @GetMapping("/test")
+    public List<Offer> getTestOffer(){
+        return List.of(
+                new Offer("Test-Offer-A"),
+                new Offer("Test-Offer-B")
+        );
+    }
 }
