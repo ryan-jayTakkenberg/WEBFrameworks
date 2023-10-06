d92b00623c5035d748f4aa9b23cd6c9cdd01901f
Mark Dijkstra <mark.dijkstra2@hva.nl>
2023-10-06 21:29:37 +0200

    setup backend

--- backend/auc-server/src/main/java/app/AucServerApplication.java
+++ backend/auc-server/src/main/java/app/AucServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AucServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AucServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AucServerApplication.class, args);
+	}
 
 }
